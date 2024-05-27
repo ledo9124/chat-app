9cb5dae2329e5e21d55a1cf53a85e9deede4e174
ledo9124 <leminhdo904@gmail.com>
2024-05-28 01:44:02 +0700

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -11,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // to parse incoming requests with JSON payloads (from req.body)
@@ -19,6 +22,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "fontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "fontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
